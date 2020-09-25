78f5a8e3085f35c8e3604ba286030798a67db1de
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-25 16:15:39 -0600

    login working finally

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -6,7 +6,7 @@ const Profile = new Schema(
     subs: [{ type: String, unique: true }],
     email: { type: String, lowercase: true, unique: true },
     name: { type: String, required: true },
-    picture: { type: String }
+    picture: { type: String },
     // NOTE If you wish to add additional public properties for profiles do so here
   },
   { timestamps: true, toJSON: { virtuals: true } }
