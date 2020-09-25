d1e864f5a973a63bc144a445a6a8f6e571012102
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-25 16:01:34 -0600

    some more auth

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "joshua-studebaker.us.auth0.com";
+export const audience = "https://buglog-studeworks.com";
+export const clientId = "CjEZgBy6tX5G0tls4fLNAIzPcO843ixs";
