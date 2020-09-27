5deab5b2e27d33372dd63403eca12803a7211e06
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-27 15:19:44 -0600

    notes server working too now

--- server/models/Note.js
+++ server/models/Note.js
@@ -7,6 +7,7 @@ const Note = new Schema(
     content: { type: String, required: true },
     bug: { type: ObjectId, ref: "Bug", required: true },
     flagged: { type: String, enum: ["pending", "completed", "rejected"] },
+    creatorEmail: { type: String, required: true },
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
