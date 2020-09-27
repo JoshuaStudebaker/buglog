c6582af4878d95d3f571bf87fb24f5c324ea5398
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-27 13:36:00 -0600

    added bug component for home page

---
+++ clientsrc/src/components/BugsComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ bugProp.title }} - {{ bugProp.description }}</p>
+</template>
+
+<script>
+export default {
+  name: "bugs-component",
+  props: ["bugProp"],
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style></style>
