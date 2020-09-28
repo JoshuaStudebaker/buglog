c47e6f5a31a75a03955094b4d40545522b447bcb
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-09-28 14:55:59 -0600

    got open closed filter on home page

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="home container-fluid d-flex flex-column">
     <div class="row">
+      <label for="toggleView">Filter</label>
+      <select class="form-control" id="toggleView" v-model="toggleView">
+        <option value="1">All</option>
+        <option value="2">Open</option>
+        <option value="3">Closed</option>
+      </select>
+    </div>
+    <div class="row" v-if="toggleView == 1">
       <bugs-component v-for="iBug in bugs" :key="iBug.id" :bugProp="iBug" />
+    </div>
+    <div class="row" v-if="toggleView == 2">
+      <bugs-component
+        v-for="iBug in bugs"
+        :key="iBug.id"
+        :bugProp="iBug"
+        v-show="!iBug.closed"
+      />
+    </div>
+    <div class="row" v-if="toggleView == 3">
+      <bugs-component
+        v-for="iBug in bugs"
+        :key="iBug.id"
+        :bugProp="iBug"
+        v-show="iBug.closed"
+      />
     </div>
   </div>
 </template>
@@ -10,6 +34,9 @@
 import bugsComponent from "../components/BugsComponent";
 export default {
   name: "home",
+  data() {
+    return { toggleView: 1 };
+  },
   components: {
     bugsComponent,
   },
